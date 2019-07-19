dc9615746a65283ea607a9ac07618cac98fc14ff
Chris Junker <elevatemedfit@gmail.com>
2019-07-19 01:03:12 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,13 +4,16 @@ class Appointment
 
   @@all = []
 
-  def initialize(date = " ",patient,doctor)
+  def initialize(date,patient,doctor)
     @date = date
     @patient = patient
     @doctor = doctor
     @@all << self
   end
 
+def date
+  @date = " "
+end
   def self.all
     @@all
   end
