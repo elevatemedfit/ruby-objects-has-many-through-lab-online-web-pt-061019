5a06b1233ca0b55ba06afd75228d36f5bccca453
Chris Junker <elevatemedfit@gmail.com>
2019-07-19 00:33:26 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,7 +14,7 @@ class Doctor
   end
 
   def new_appointment(patient, date)
-      self << Appointment.new(patient,self,date) 
+      appointment = Appointment.new(patient,self,date) 
   end
 
   def appointments
