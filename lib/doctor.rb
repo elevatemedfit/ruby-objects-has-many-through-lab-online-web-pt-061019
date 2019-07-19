c6fc17548cad504607cab4828e0d5dd1063ba0c6
Chris Junker <elevatemedfit@gmail.com>
2019-07-19 00:42:48 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,16 +14,16 @@ class Doctor
   end
 
   def new_appointment(patient, date)
-      appointment = Appointment.new(patient,self,date)
+      Appointment.new(patient,self,date)
   end
 
-  def appointments#meals
+  def appointments#meals>>
       Appointment.all.select do |appointment|
       appointment.doctor == self
     end
   end
 
-   def patients#waiters
+   def patients#waiters>>
       appointments.map do |appointment|
       appointment.patient
    end
